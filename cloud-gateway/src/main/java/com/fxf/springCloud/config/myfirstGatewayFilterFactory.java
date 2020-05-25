762c2c3395e4ddd5212e78e0507e1a1bca94040c
fanxiaofeng <1366393304@qq.com>
2020-05-25 11:09:20 +0800

    添加自定义过滤器工厂

---
+++ cloud-gateway/src/main/java/com/fxf/springCloud/config/myfirstGatewayFilterFactory.java
@@ -0,0 +1,70 @@
+package com.fxf.springCloud.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author FXF
+ * @create 2020-05-21 17:24
+ */
+@Component
+public class myfirstGatewayFilterFactory extends AbstractGatewayFilterFactory<myfirstGatewayFilterFactory.Config> {  //自定义过滤器工厂类时，按照Spring Cloud Stream的约定，类名须为“过滤器名（本文例中：PreLog）” + GatewayFilterFactory
+
+
+    private static final Log log = LogFactory.getLog(GatewayFilter.class);
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+    private static final String KEY = "withParams";
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(KEY);
+    }
+
+    public myfirstGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                        if (startTime != null) {
+                            StringBuilder sb = new StringBuilder(exchange.getRequest().getURI().getRawPath())
+                                    .append(": ")
+                                    .append(System.currentTimeMillis() - startTime)
+                                    .append("ms");
+                            if (config.isWithParams()) {
+                                sb.append(" params:").append(exchange.getRequest().getQueryParams());
+                            }
+                            log.info(sb.toString());
+                        }
+                    })
+            );
+        };
+    }
+
+
+    public static class Config {
+
+        private boolean withParams;
+
+        public boolean isWithParams() {
+            return withParams;
+        }
+
+        public void setWithParams(boolean withParams) {
+            this.withParams = withParams;
+        }
+
+    }
+}
