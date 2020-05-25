b22904b19330647126c789f8185b0ca277d15d8d
fanxiaofeng <1366393304@qq.com>
2020-05-25 11:08:34 +0800

    添加自定义过滤器工厂

--- cloud-gateway/src/main/java/com/fxf/springCloud/config/myRoutes.java
+++ cloud-gateway/src/main/java/com/fxf/springCloud/config/myRoutes.java
@@ -1,5 +1,6 @@
 package com.fxf.springCloud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class myRoutes {
 
+    /**
+     * 测试添加自定义过滤器
+     * @param builder
+     * @return
+     */
     @Bean
     public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
         RouteLocator route=builder.routes()
@@ -28,5 +34,32 @@ public class myRoutes {
         return route;
 
     }
+
+
+    /**
+     * 测试自定义过滤器工厂方式，添加过滤器
+     * 自定义过滤器工厂可通过Yaml配置或者通过这种Bean方式添加
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator customerRouteLocator2(RouteLocatorBuilder builder) {
+        myfirstGatewayFilterFactory.Config config=new myfirstGatewayFilterFactory.Config();
+        config.setWithParams(true);
+        RouteLocator route=builder.routes()
+                .route(r -> r.path("/customer2/**")
+                        .filters(f -> f.filter(new myfirstGatewayFilterFactory().apply(config))
+                                .addResponseHeader("X-Response-Default-Foo", "Default-Bar")
+                                .stripPrefix(1) //路由时转发时，去掉第一个前缀。即customer
+                        )
+                        .uri("lb://cloud-nacos-consumer")
+                        .order(0)
+                        .id("customer_filter_router")
+                ).build();
+
+        return route;
+
+    }
+
 
 }
